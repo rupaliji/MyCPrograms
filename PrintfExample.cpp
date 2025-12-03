f0edef8838a084522f11e2516c448613b89c54b6
Rupali Verma <rupaliverma849@gmail.com>
2025-12-04 00:01:10 +0530

    Add files via upload

---
+++ PrintfExample.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello");	
+	
+	
+	printf("\nRup\nali Verma");
+	printf("\n\n\n\nI am a \ns\nt\nu\nd\ne\nn\nt.");
+	printf("He told me that \"He is happy\".");
+	return 0;
+}
